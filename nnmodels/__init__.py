5b0e62d3ab3cd9e7240373f5192f4bc06f5f2b38
Piskala Dilipkumar <santhoshkumar.piskaladilipkumar@dlr.de>
2026-03-10 16:58:49 +0100

    base class added

--- nnmodels/__init__.py
+++ nnmodels/__init__.py
@@ -1,5 +1,6 @@
 from .mlp_neuralode import NeuralODE
 from .aug_mlp_neuralode import AugNeuralODE
+from .feedforward import FeedForward
 
-__all__ = ["NeuralODE", "AugNeuralODE"]
+__all__ = ["NeuralODE", "AugNeuralODE", "FeedForward"]
 
